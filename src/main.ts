a20630f8f4bbfd047bad605c3c3805d310be1d0a
MTrun <1262327911@qq.com>
2021-05-14 21:54:25 +0800

    feat: 新增图标文件,引入echarts

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import dataV from '@jiaminghi/data-view';
+// 引入全局css
+import './assets/scss/style.scss';
+// 引入图表（所有图标见 icon 目录下的 demo_index.html）
+import './assets/icon/iconfont.css'
+// 引入echarts
+import * as echarts from 'echarts';
 
 const app = createApp(App)
+
+// 全局挂载
+app.config.globalProperties.$echarts = echarts
 app.use(dataV)
 app.use(store)
 app.use(router)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    home
+    <dv-full-screen-container>
+      <dv-border-box-12 style="width: 200px; height: 200px">dv-border-box-12</dv-border-box-12>
+    </dv-full-screen-container>
   </div>
 </template>
 
@@ -8,12 +10,10 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  setup () {
+  setup() {
     return {}
   }
 })
 </script>
 
-<style scoped>
-
-</style>
+<style scss scoped></style>
