605d592a6536c99a90ef01149292f864f3e6c9f3
MTrun <1262327911@qq.com>
2021-05-14 09:02:25 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import dataV from '@jiaminghi/data-view';
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+app.use(dataV)
+app.use(store)
+app.use(router)
+app.mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    home
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent } from 'vue'
 
-@Options({
-  components: {
-    HelloWorld,
-  },
+export default defineComponent({
+  setup () {
+    return {}
+  }
 })
-export default class Home extends Vue {}
 </script>
+
+<style scoped>
+
+</style>
